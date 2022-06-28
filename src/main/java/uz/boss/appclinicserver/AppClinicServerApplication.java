5c68e3196e5b4283c949b7f9c5e20502903ceb1e
Muhammad0224 <murtazayevmuhammad@gmail.com>
2022-06-28 11:04:17 +0500

    first init

---
+++ src/main/java/uz/boss/appclinicserver/AppClinicServerApplication.java
@@ -0,0 +1,13 @@
+package uz.boss.appclinicserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppClinicServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppClinicServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/boss/appclinicserver/AppClinicServerApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.boss.appclinicserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppClinicServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
